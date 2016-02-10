aa3c0aafa9cd7868151e7ec3847b38e95897695a
Marc A. Anderson <marc@marcadam.com>
2016-02-10 01:18:25 -0800

    Initial commit

---
+++ Yelp/BusinessCell.swift
@@ -0,0 +1,73 @@
+//
+//  BusinessCell.swift
+//  Yelp
+//
+//  Created by Marc Anderson on 2/9/16.
+//  Copyright © 2016 Timothy Lee. All rights reserved.
+//
+
+import UIKit
+
+class BusinessCell: UITableViewCell {
+
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var distanceLabel: UILabel!
+    @IBOutlet weak var reviewsCountLabel: UILabel!
+    @IBOutlet weak var addressLabel: UILabel!
+    @IBOutlet weak var categoriesLabel: UILabel!
+    @IBOutlet weak var thumbImageView: UIImageView!
+    @IBOutlet weak var ratingImageView: UIImageView!
+
+    var business: Business! {
+        didSet {
+            nameLabel.text = business.name
+            distanceLabel.text = business.distance
+            if let reviewsCount = business.reviewCount {
+                reviewsCountLabel.text = "\(reviewsCount) Reviews"
+            }
+            addressLabel.text = business.address
+            categoriesLabel.text = business.categories
+            if let imageURL = business.imageURL {
+                thumbImageView.setImageWithURL(imageURL)
+            }
+            if let imageURL = business.ratingImageURL {
+                ratingImageView.setImageWithURL(imageURL)
+            }
+        }
+    }
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+
+        thumbImageView.layer.cornerRadius = 4
+        thumbImageView.clipsToBounds = true
+
+//        nameLabel.preferredMaxLayoutWidth = nameLabel.frame.size.width
+//        addressLabel.preferredMaxLayoutWidth = addressLabel.frame.size.width
+    }
+
+    override func layoutSubviews() {
+        super.layoutSubviews()
+
+//        nameLabel.preferredMaxLayoutWidth = nameLabel.frame.size.width
+//        addressLabel.preferredMaxLayoutWidth = addressLabel.frame.size.width
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+    override func prepareForReuse() {
+        nameLabel.text = nil
+        distanceLabel.text = nil
+        reviewsCountLabel.text = nil
+        addressLabel.text = nil
+        categoriesLabel.text = nil
+        thumbImageView.image = nil
+        ratingImageView.image = nil
+    }
+
+}
---
+++ Yelp/BusinessesViewController.swift
@@ -0,0 +1,79 @@
+//
+//  BusinessesViewController.swift
+//  Yelp
+//
+//  Created by Timothy Lee on 4/23/15.
+//  Copyright (c) 2015 Timothy Lee. All rights reserved.
+//
+
+import UIKit
+
+class BusinessesViewController: UIViewController {
+
+    var businesses: [Business]!
+    
+    @IBOutlet weak var tableView: UITableView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        tableView.rowHeight = UITableViewAutomaticDimension
+        tableView.estimatedRowHeight = 90
+
+        Business.searchWithTerm("Thai", completion: { (businesses: [Business]!, error: NSError!) -> Void in
+            self.businesses = businesses
+            self.tableView.reloadData()
+
+            for business in businesses {
+                print(business.name!)
+                print(business.address!)
+            }
+        })
+
+/* Example of Yelp search with more search options specified
+        Business.searchWithTerm("Restaurants", sort: .Distance, categories: ["asianfusion", "burgers"], deals: true) { (businesses: [Business]!, error: NSError!) -> Void in
+            self.businesses = businesses
+            
+            for business in businesses {
+                print(business.name!)
+                print(business.address!)
+            }
+        }
+*/
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
+
+extension BusinessesViewController: UITableViewDataSource, UITableViewDelegate {
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if let businesses = businesses {
+            return businesses.count
+        } else {
+            return 0
+        }
+    }
+
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("BusinessCell", forIndexPath: indexPath) as! BusinessCell
+
+        cell.business = businesses[indexPath.row]
+
+        return cell
+    }
+
+}
