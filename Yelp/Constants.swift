28cebe2381fad43344c3eb90ce3e12a936849838
Marc A. Anderson <marc@marcadam.com>
2016-02-13 20:11:12 -0800

    Factor default location into Constants file

---
+++ Yelp/Constants.swift
@@ -0,0 +1,16 @@
+//
+//  Constants.swift
+//  Yelp
+//
+//  Created by Marc Anderson on 2/13/16.
+//  Copyright © 2016 Timothy Lee. All rights reserved.
+//
+
+import Foundation
+
+struct Constants {
+    struct DefaultLocation {
+        static let latitude = 37.785771
+        static let longitude = -122.406165
+    }
+}
