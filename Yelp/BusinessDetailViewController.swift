8d3d58383128b47283cfacb750b6b8fb652e1ae9
Marc A. Anderson <marc@marcadam.com>
2016-05-21 22:33:33 -0700

    Deselect cell after selection

--- Yelp/BusinessDetailViewController.swift
+++ Yelp/BusinessDetailViewController.swift
@@ -180,6 +180,10 @@ extension BusinessDetailViewController: UITableViewDataSource, UITableViewDelega
             return cell!
         }
     }
+
+    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        tableView.deselectRowAtIndexPath(indexPath, animated: true)
+    }
 }
 
 extension BusinessDetailViewController: UIScrollViewDelegate {
